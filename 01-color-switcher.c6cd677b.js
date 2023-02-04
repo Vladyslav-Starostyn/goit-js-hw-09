22f884d0130086d52b75277112131bc1f22fa4bb
Vladyslav-Starostyn <98741635+Vladyslav-Starostyn@users.noreply.github.com>
2023-02-04 18:07:36 +0000

    Deploying to gh-pages from @ Vladyslav-Starostyn/goit-js-hw-09@d4b0a112dc33c326065fb0478734271799c77f2d 🚀

---
+++ 01-color-switcher.c6cd677b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0;let o=null;function n(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}console.log(t),console.log(e),t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n(),o=setInterval(n,1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.c6cd677b.js.map
