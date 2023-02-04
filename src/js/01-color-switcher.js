d4b0a112dc33c326065fb0478734271799c77f2d
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2023-02-04 20:07:00 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const buttonStartEl = document.querySelector('button[data-start]');
+const buttonStopEl = document.querySelector('button[data-stop]');
+buttonStopEl.disabled = true;
+const COLOR_INTERVAL = 1000;
+let timerId = null;
 
+console.log(buttonStartEl);
+console.log(buttonStopEl);
+
+buttonStartEl.addEventListener('click', onButtonStartClick);
+buttonStopEl.addEventListener('click', onButtonStopClick);
+
+function onButtonStartClick() {
+  buttonStartEl.disabled = true;
+  buttonStopEl.disabled = false;
+
+  onBodyColor();
+
+  timerId = setInterval(onBodyColor, COLOR_INTERVAL);
+}
+
+function onButtonStopClick() {
+  clearInterval(timerId);
+  buttonStartEl.disabled = false;
+  buttonStopEl.disabled = true;
+}
+
+function onBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
