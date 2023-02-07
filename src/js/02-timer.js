547392cba453aece136f7c9b4a6a6322ef4cfc45
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2023-02-07 15:05:01 +0200

    task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,21 +6,13 @@ const textEl = document.getElementById('datetime-picker');
 const buttonStartEl = document.querySelector('button[data-start]');
 const timerEl = document.querySelector('.timer');
 const daysEl = document.querySelector('[data-days]');
-const hours = document.querySelector('[data-hours]');
+const hoursEl = document.querySelector('[data-hours]');
 const minutesEl = document.querySelector('[data-minutes]');
 const secondsEi = document.querySelector('[data-seconds]');
 
 buttonStartEl.disabled = true;
 
 buttonStartEl.addEventListener('click', onButtonClick);
-
-console.log(textEl);
-console.log(buttonStartEl);
-console.log(daysEl);
-console.log(hours);
-console.log(minutesEl);
-console.log(secondsEi);
-console.log(timerEl);
 
 const options = {
   enableTime: true,
@@ -41,19 +33,14 @@ const options = {
 flatpickr(textEl, options);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
@@ -67,9 +54,19 @@ function onButtonClick() {
   timerId = setInterval(() => {
     let counter = new Date(textEl.value) - new Date();
     buttonStartEl.disabled = true;
-  });
+    if (counter >= 0) {
+      let timerObj = convertMs(counter);
+      daysEl.textContent = addLeadingZero(timerObj.days);
+      hoursEl.textContent = addLeadingZero(timerObj.hours);
+      minutesEl.textContent = addLeadingZero(timerObj.minutes);
+      secondsEi.textContent = addLeadingZero(timerObj.seconds);
+      if (counter < 10000) {
+        timerEl.style.color = 'tomato';
+      }
+    } else {
+      Notiflix.Notify.success('Countdown finished');
+      timerEl.style.color = 'black';
+      clearInterval(timerId);
+    }
+  }, 1000);
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
